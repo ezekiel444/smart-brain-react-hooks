992ed3e74ac8628e126f6bd63488ae6be5706304
Ezekiel <ezekielmatomi588@gmail.com>
2020-05-10 15:43:36 +0200

    Updates

---
+++ precache-manifest.67942ae49dd79c9763e17ef91a88cd00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a9da80dbf006c154f42d92a0951345",
+    "url": "/face-recognition-with-react-hooks-/index.html"
+  },
+  {
+    "revision": "8a2fd1f43321804a42e3",
+    "url": "/face-recognition-with-react-hooks-/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "9604c9eace9dd9b8f905",
+    "url": "/face-recognition-with-react-hooks-/static/css/main.b03f30d5.chunk.css"
+  },
+  {
+    "revision": "8a2fd1f43321804a42e3",
+    "url": "/face-recognition-with-react-hooks-/static/js/2.242e0813.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-recognition-with-react-hooks-/static/js/2.242e0813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9604c9eace9dd9b8f905",
+    "url": "/face-recognition-with-react-hooks-/static/js/main.b7d11348.chunk.js"
+  },
+  {
+    "revision": "3ffa3976b0e0f1d7993b",
+    "url": "/face-recognition-with-react-hooks-/static/js/runtime-main.12e1f26e.js"
+  },
+  {
+    "revision": "469fb591e66a4a67550199831ce19645",
+    "url": "/face-recognition-with-react-hooks-/static/media/brain.469fb591.jpg"
+  }
+]);
